f220b1b2164dde5e7b496b895aa1c26a0f3da203
Vinicius Martin <viniciusmartindeoliveira@hotmail.com>
2025-08-27 00:08:26 +0000

    feat: criando a função principal

--- FreeFire.c
+++ FreeFire.c
@@ -16,3 +16,37 @@ void adicionarItem();
 void removerItem();
 void listarItens();
 void limparBufferEntrada();
+
+int main() {
+    int opcao;
+
+    do {
+        printf("\n--- Mochila de Sobrevivencia ---\n");
+        printf("1. Adicionar item\n");
+        printf("2. Remover item\n");
+        printf("3. Listar itens\n");
+        printf("4. Sair\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+        limparBufferEntrada();
+
+        switch (opcao) {
+            case 1:
+                adicionarItem();
+                break;
+            case 2:
+                removerItem();
+                break;
+            case 3:
+                listarItens();
+                break;
+            case 4:
+                printf("Fechando o sistema da mochila. Ate a proxima!\n");
+                break;
+            default:
+                printf("Opcao invalida. Tente novamente.\n");
+        }
+    } while (opcao != 4);
+
+    return 0;
+}
